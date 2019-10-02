2995e00f16de3589a26828b0d593d9ab755142e1
Tao309 <tao309@mail.ru>
2019-10-02 11:19:46 +0300

    Prepare Class

---
+++ source/product.php
@@ -0,0 +1,47 @@
+<?php
+
+class Product {
+    private $title = 'Яблоки';
+    private $price = 200;
+    private $category = 'Фрукты';
+
+    public function __construct(string $title = null, int $price = null, string $category = null)
+    {
+        if($title)
+        {
+            $this->title = $title;
+        }
+
+        if($price)
+        {
+            $this->price = $price;
+        }
+
+        if($category)
+        {
+            $this->category = $category;
+        }
+    }
+
+    public function getTitle():string
+    {
+        return $this->title;
+    }
+    public function getPrice():int
+    {
+        return $this->price;
+    }
+    public function getCategory():string
+    {
+        return $this->category;
+    }
+
+    public function setPrice(int $value)
+    {
+        $this->price = $value;
+    }
+    public function setCategory(string $value)
+    {
+        $this->category = $value;
+    }
+}
